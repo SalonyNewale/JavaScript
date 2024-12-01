34fc339daf4109ba7c7288931db8d788f3624abf
Salony Rajendra Newale <128674306+SalonyNewale@users.noreply.github.com>
2024-12-01 09:48:43 +0000

    variables file commit

---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 11
+let email = "salony@gmail.com";
+var password = "1234"
+random = "Amravati"
+let currentState;
+
+
+console.log(accountId);
+console.table([accountId,email,password,random,currentState])
+
+
+
+
