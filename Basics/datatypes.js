7469a99194cd1f202a4123af193649c71e800903
Salony Rajendra Newale <128674306+SalonyNewale@users.noreply.github.com>
2024-12-01 10:09:11 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version 
+
+//alert("hello") //we are using nodejs, not browser
+
+let name = "Salony"
+let age = 22
+let isLoggedIn = false
+
+//null is standalone value => typeof is object
+//undefined is value not assigned => typeof is undefined
+//symbol hwhen unique
+//object 
+
+console.log(typeof "Salony");
